9a834bd25179111cf4efe06b3520b75370023d00
Nofaxe54 <42420445+Nofaxe54@users.noreply.github.com>
2018-10-06 20:22:30 +0200

    frm

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
-<php
+<?php
 
 
 ?>
-  testggg
+
+<form class="" action="sendMsg.php" method="post">
+  <input type="text" name="" value="" placeholder="Pseudo">
+  <input type="text" name="" value="Message">
+</form>
